017799d7eb517002582548f92020c6c5c9b3f1cf
Mike-R-A <michaelashford@Michaels-iMac.home>
2017-08-30 08:32:10 +0100

    decrease food speed running away slightly

--- src/thing.ts
+++ src/thing.ts
@@ -65,8 +65,8 @@ class Thing extends Drawable {
                 }
 
                 if (otherThing instanceof Creature || distanceToOtherThing <= (this.width + otherThing.width) / 2) {
-                    this.x = this.x - xToOther * 2;
-                    this.y = this.y - yToOther * 2;
+                    this.x = this.x - xToOther * 1.1;
+                    this.y = this.y - yToOther * 1.1;
                 } else {
                     this.x = this.x + xToOther;
                     this.y = this.y + yToOther;
